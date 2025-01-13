432cb0e18e411146986ef820ba419302b0c47fc2
Revanth sahu <rohansahutelukula@gmail.com>
2025-01-14 01:43:34 +0530

    initialised

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const app = require("./app");
+const server=http.createServer(app)
+const port = process.env.PORT || 3000;
+
+server.listen(port,()=>{
+    console.log(`running on port ${port}`)
+})
